ca0fadb93ab4b4808e81f1e910c32c5a9d120ef0
J3rry <ge58ler@mytum.de>
2022-12-07 22:39:42 +0100

    text completion fix

--- main.py
+++ main.py
@@ -148,7 +148,6 @@ async def imagine(interaction: discord.Interaction, prompt: str):
 @bot.tree.command(name='complete', description='Generate a text using a prompt')
 @app_commands.describe(prompt='description')
 async def complete(interaction: discord.Interaction, prompt: str):
-    await interaction.response.send_message(f"Let the workers work on that...")
     try:
         response = openai.Completion.create(model="text-davinci-003", prompt=prompt)
     except openai.InvalidRequestError:
