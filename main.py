52bb36ebd27c8422d2a40c2746d342a2ba913142
J3rry <ge58ler@mytum.de>
2022-12-07 22:38:32 +0100

    text completion fix

--- main.py
+++ main.py
@@ -152,8 +152,9 @@ async def complete(interaction: discord.Interaction, prompt: str):
     try:
         response = openai.Completion.create(model="text-davinci-003", prompt=prompt)
     except openai.InvalidRequestError:
-        return await interaction.channel.send(f"*{prompt}* cannot be worked out by the workers. Try another prompt that is less nsfw.")
-    await interaction.channel.send(response)
+        return await interaction.response.send_message(f"*{prompt}* cannot be worked out by the workers. Try another prompt that is less nsfw.")
+    text = response['choices'][0]['text']
+    await interaction.response.send_message(text)
 @bot.tree.error
 async def role_error_catch(interaction: discord.Interaction, error):
     if isinstance(error, app_commands.MissingRole):
