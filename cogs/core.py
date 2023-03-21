aa6f76e40ae996c03a13ccf0ffcd4db94a929892
J3rry <ge58ler@mytum.de>
2023-03-21 17:02:43 +0100

    test

--- cogs/core.py
+++ cogs/core.py
@@ -175,7 +175,7 @@ class Core(commands.Cog):
         game_choices.append(
             discord.app_commands.Choice(name=data['games'][len(data['games']) - 1]['name'], value=len(data['games']) - 1))
         await interaction.response.send_message(f"**{game}** was added to the ping system")
-        run_reload('cogs')
+        await run_reload('cogs')
 
 
     @app_commands.command(name='pingremovegame', description='Remove a game fromd the ping list')
