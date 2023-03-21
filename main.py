e17fc56f91c594c59e048c64719cbc5cea0f6641
J3rry <ge58ler@mytum.de>
2023-03-21 16:47:34 +0100

    removed reload

--- main.py
+++ main.py
@@ -77,10 +77,6 @@ async def on_member_remove(member: discord.Member):
      
 bot.run(data['token'])  
 
-@bot.command()
-@commands.is_owner()
-async def reload(ctx, extension):
-    bot.reload_extension(f"cogs.{extension}")
-    await ctx.send('test')
+
         
         
